e4351c824690db15323c82c107a0d6ce5339acf3
Spawn Colen <scolen@student.21-school.ru>
2021-05-23 09:09:36 +0300

    finish day0

---
+++ Day 0/ex00/main.c/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace main.c
+{
+    class Program
+    {
+        static int OutputErrorMessage()
+        {
+            Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+            return (1);
+        }
+
+        static double ThreatmentRepaymentDecreaseSum(double sum, double rate, int term, int selectedMonth, double payment)
+        {
+            double annualInterestRate = rate / 12.0f / 100.0f;
+            DateTime date = DateTime.Today;
+            double annuityPayment = (sum * annualInterestRate * Math.Pow((1 + annualInterestRate), term)
+                / (Math.Pow((1 + annualInterestRate), term) - 1));
+            double overpayment = 0;
+            for (int i = 1; i <= term; ++i)
+            {
+                double procent = (sum * rate * DateTime.DaysInMonth(date.Year, (date.Month + i - 2) % 12 + 1))
+                                / (100 * (DateTime.IsLeapYear(date.Year) ? 366 : 365));
+                overpayment += procent;
+                sum -= annuityPayment - procent;
+                if (i == selectedMonth)
+                {
+                    sum -= payment;
+                    annuityPayment = (sum * annualInterestRate * Math.Pow((1 + annualInterestRate), term - i)
+                / (Math.Pow((1 + annualInterestRate), term - i) - 1));
+                }
+            }
+            return (overpayment);
+        }
+        static double ThreatmentRepaymentDecreaseTerm(double sum, double rate, int term, int selectedMonth, double payment)
+        {
+            double annualInterestRate = rate / 12.0f / 100.0f;
+            DateTime date = DateTime.Today;
+            double annuityPayment = (sum * annualInterestRate * Math.Pow((1 + annualInterestRate), term)
+                / (Math.Pow((1 + annualInterestRate), term) - 1));
+            double overpayment = 0;
+            for (int i = 1; i <= term; ++i)
+            {
+                double procent = (sum * rate * DateTime.DaysInMonth(date.Year, (date.Month + i - 2) % 12 + 1))
+                                / (100 * (DateTime.IsLeapYear(date.Year) ? 366 : 365));
+                overpayment += procent;
+                sum -= annuityPayment - procent;
+                if (i == selectedMonth)
+                {
+                    sum -= payment;
+                }
+                if (sum <= 0)
+                    break;
+            }
+            return (overpayment);
+        }
+
+        static int Main(string[] args)
+        {
+            // Сумма кредита, р
+            double sum = 0;
+            // Годовая процентная ставка, %
+            double rate = 0;
+            // Количество месяцев кредита
+            int term = 0;
+            // Номер месяца кредита, в котором вносится досрочный платёж
+            int selectedMonth = 0;
+            // Сумма досрочного платежа, р
+            double payment = 0;
+            int lengthArray = args.GetLength(0);
+            bool checkValide = false;
+
+            if (lengthArray == 5)
+            {
+                checkValide = Double.TryParse(args[0], out sum);
+                if (checkValide == false)
+                    return (OutputErrorMessage());
+                checkValide = Double.TryParse(args[1], out rate);
+                if (checkValide == false)
+                    return (OutputErrorMessage());
+                checkValide = Int32.TryParse(args[2], out term);
+                if (checkValide == false)
+                    return (OutputErrorMessage());
+                checkValide = Int32.TryParse(args[3], out selectedMonth);
+                if (checkValide == false)
+                    return (OutputErrorMessage());
+                checkValide = Double.TryParse(args[4], out payment);
+                if (checkValide == false)
+                    return (OutputErrorMessage());
+                double overpayment1 = ThreatmentRepaymentDecreaseSum(sum, rate, term, selectedMonth, payment);
+                double overpayment2 = ThreatmentRepaymentDecreaseTerm(sum, rate, term, selectedMonth, payment);
+                System.Console.WriteLine("Переплата при уменьшении платежа: {0:F2}р.", overpayment1);
+                System.Console.WriteLine("Переплата при уменьшении срока: {0:F2}р.", overpayment2);
+                if ((int)overpayment1 > (int)overpayment2)
+                    System.Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа на {0:F2}р.", overpayment1 - overpayment2);
+                else if ((int)overpayment1 < (int)overpayment2)
+                    System.Console.WriteLine("Уменьшение платежа выгоднее уменьшения срока на {0:F2}р.", overpayment2 - overpayment1);
+                else
+                    System.Console.WriteLine("Переплата одинакова в обоих вариантах.");
+            }
+            else
+            {
+                return (OutputErrorMessage());
+            }
+            return (0);
+        }
+    }
+}
---
+++ Day 0/ex01/ex01/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ex01
+{
+    class Program
+    {
+        static int Rec(int i, int j, string a, string b)
+        {
+            if (i == 0 || j == 0)
+                return (Math.Max(i, j));
+            else if (a[i - 1] == b[j - 1])
+                return (Rec(i - 1, j - 1, a, b));
+            else
+            {
+                return (1 + 
+                    Math.Min(
+                            Math.Min(Rec(i, j - 1, a, b), Rec(i - 1, j, a, b)),
+                                Rec(i - 1, j - 1, a, b)));
+            }
+        }
+
+        static int DistanceLevenshtein(string a, string b)
+        {
+            return (Rec(a.Length, b.Length, a, b));
+        }
+        static int OutputErrorMessage(string message)
+        {
+            int error = 1;
+            System.Console.WriteLine("{0}", message);
+            return (error);
+        }
+        static int Main(string[] args)
+        {
+            string nameUser;
+            int lengthString;
+
+            Console.Write("Enter name: ");
+            nameUser = Console.ReadLine();
+            lengthString = nameUser.Length;
+
+            if (lengthString == 0)
+                return (OutputErrorMessage("Your name was not found."));
+
+            string[] allNames = File.ReadAllLines("us.txt");
+            if (allNames == null)
+                return (OutputErrorMessage("File not exist!"));
+            string closeWord = allNames[0];
+            int minDistance = DistanceLevenshtein(allNames[0], nameUser);
+            for (int i = 0; i < allNames.Length; i++) 
+            {
+                int dist = DistanceLevenshtein(allNames[i], nameUser);
+                if (dist < 3)
+                {
+                    // System.Console.WriteLine("name = {0}, Расстояние левенштейна: {1}", allNames[i], dist);
+                    if (allNames[i][0] == nameUser[0])
+                    {
+                        if (dist < minDistance)
+                        {
+                            minDistance = dist;
+                            closeWord = allNames[i];
+                        }
+                        // System.Console.WriteLine("name = {0}, Расстояние левенштейна: {1}",
+                        //     allNames[i], dist);
+                    }
+                }
+            }
+            System.Console.WriteLine("Did you mean \"{0}\"? Y/N", closeWord);
+            char answer; 
+            bool isChar = Char.TryParse(Console.ReadLine(), out answer);
+            if (isChar == true && answer == 'Y')
+                System.Console.WriteLine("Hello, {0}!", closeWord);
+            else
+                return (OutputErrorMessage("Your name was not found."));
+            return (0);
+        }
+    }
+}
